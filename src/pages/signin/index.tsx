92f4ff23d7a2ccefe87a729cdbd49762d59b9b4a
Fabio Niglio <fabioniglio@gmail.com>
2020-06-08 10:59:44 +0200

    change language text signin page

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -79,7 +79,7 @@ const SignIn: React.FC = () => {
           <img src={logoImg} alt="GoBarber" />
 
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu Logon</h1>
+            <h1>Login</h1>
             <Input name="email" icon={FiMail} placeholder="E-mail" />
             <Input
               name="password"
